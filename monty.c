dcfb298a4fd6bac083f6f57cfa27310e38bcc753
hileamlak <hilea.buisness@gmail.com>
2020-12-19 07:53:28 +0000

    Add memroy utils

--- monty.c
+++ monty.c
@@ -1,1 +1,38 @@
 #include "monty.h"
+
+
+/**
+ * main - entry point of the intepreter
+ * @argc: argument count
+ * @argv: argument list
+ * Return: 0 on success, and error messages as specified
+ in error.h
+ */
+int main(int argc, char **argv)
+{
+	int fd = -1;
+
+
+	/*check if there is correct argument number*/
+	if (argc != 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*safely open the file*/
+	fd = sopen(argv[1]);
+
+	/*Read one line at a time*/
+	/*Check if it is correct*/
+	/*Execute that line*/
+	while ()
+	{
+
+	}
+
+	/*Close the file before exiting*/
+	close(fd);
+	exit(EXIT_SUCCESS);
+}
+
